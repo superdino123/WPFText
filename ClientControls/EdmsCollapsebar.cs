1821640a8f36aac7c5b2f38b0b08bcf1cf571cf0
diplant <1119674493@qq.com>
2017-07-17 14:30:29 +0800

    初步模仿第一阶段

---
+++ ClientControls/EdmsCollapsebar.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+namespace ClientControls
+{
+    /// <summary>
+    /// </summary>
+    public class EdmsCollapsebar : Expander
+    {
+        public enum ModeOptions
+        {
+            Normal,
+            WrapOnly
+        }
+
+        // Using a DependencyProperty as the backing store for GripOffset.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GripOffsetProperty =
+            DependencyProperty.Register("GripOffset", typeof(double), typeof(EdmsCollapsebar));
+
+        // Using a DependencyProperty as the backing store for CollapsedSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CollapsedSizeProperty =
+            DependencyProperty.Register("CollapsedSize", typeof(GridLength), typeof(EdmsCollapsebar), new UIPropertyMetadata(new GridLength(0, GridUnitType.Auto)));
+
+        // Using a DependencyProperty as the backing store for Mode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ModeProperty =
+            DependencyProperty.Register("Mode", typeof(ModeOptions), typeof(EdmsCollapsebar), new UIPropertyMetadata(ModeOptions.Normal));
+
+
+        private GridLength _startupSize;
+        private GridLength SizeCollapsedBackup = new GridLength(200);
+
+        private GridLength? SizeExpandedBackup;
+
+
+        static EdmsCollapsebar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EdmsCollapsebar), new FrameworkPropertyMetadata(typeof(EdmsCollapsebar)));
+        }
+
+        public EdmsCollapsebar()
+        {
+            Expanded += Sidebar_Expanded;
+            Collapsed += Sidebar_Collapsed;
+        }
+
+        public GridLength StartupSize
+        {
+            get { return _startupSize; }
+            set
+            {
+                SizeExpandedBackup = value;
+                _startupSize = value;
+                SetCurrentValue(StoredWidthProperty, value);
+            }
+        }
+
+
+        /// <summary>
+        ///     The offset of the toggle grip
+        /// </summary>
+        public double GripOffset
+        {
+            get { return (double)GetValue(GripOffsetProperty); }
+            set { SetValue(GripOffsetProperty, value); }
+        }
+
+
+        /// <summary>
+        ///     The size of the collapsed sidebar
+        /// </summary>
+        public GridLength CollapsedSize
+        {
+            get { return (GridLength)GetValue(CollapsedSizeProperty); }
+            set { SetValue(CollapsedSizeProperty, value); }
+        }
+
+
+        public ModeOptions Mode
+        {
+            get { return (ModeOptions)GetValue(ModeProperty); }
+            set { SetValue(ModeProperty, value); }
+        }
+
+        //private GridLength MinWidthBackup = new GridLength(0);
+
+        private void Sidebar_Collapsed(object sender, RoutedEventArgs e)
+        {
+            if (Mode == ModeOptions.Normal)
+            {
+                if (ExpandDirection == ExpandDirection.Left || ExpandDirection == ExpandDirection.Right)
+                {
+                    SizeExpandedBackup = StoredWidth = new GridLength(ActualWidth);
+                    //  MinWidthBackup = MinWidth;
+                    SetCurrentValue(StoredWidthProperty, GridLength.Auto);
+                }
+                if (ExpandDirection == ExpandDirection.Down || ExpandDirection == ExpandDirection.Up)
+                {
+                    SizeExpandedBackup = StoredHeight = new GridLength(ActualHeight);
+                    SetCurrentValue(StoredHeightProperty, GridLength.Auto);
+                }
+            }
+        }
+
+        private void Sidebar_Expanded(object sender, RoutedEventArgs e)
+        {
+            if (Mode == ModeOptions.Normal)
+            {
+                if (ExpandDirection == ExpandDirection.Left || ExpandDirection == ExpandDirection.Right)
+                {
+                    SizeCollapsedBackup = StoredWidth;
+                    if (SizeExpandedBackup == null)
+                    {
+                        SizeExpandedBackup = new GridLength(200);
+                    }
+                    SetCurrentValue(StoredWidthProperty, SizeExpandedBackup);
+                }
+                if (ExpandDirection == ExpandDirection.Down || ExpandDirection == ExpandDirection.Up)
+                {
+                    SizeCollapsedBackup = StoredHeight;
+                    if (SizeCollapsedBackup == null)
+                    {
+                        SizeExpandedBackup = new GridLength(200);
+                    }
+                    SetCurrentValue(StoredHeightProperty, SizeExpandedBackup);
+                }
+
+            }
+        }
+
+        #region DependencyProperty StoredWidth
+
+        /// <summary>
+        /// </summary>
+        public GridLength StoredWidth
+        {
+            get { return (GridLength)GetValue(StoredWidthProperty); }
+            set { SetValue(StoredWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StoredWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StoredWidthProperty =
+            DependencyProperty.RegisterAttached("StoredWidth", typeof(GridLength), typeof(EdmsCollapsebar));
+
+        #endregion
+
+
+        #region DependencyProperty StoredHeight
+        public static readonly DependencyProperty StoredHeightProperty = DependencyProperty.Register(
+            "StoredHeight", typeof(GridLength), typeof(EdmsCollapsebar), new PropertyMetadata(default(GridLength)));
+
+        public GridLength StoredHeight
+        {
+            get { return (GridLength)GetValue(StoredHeightProperty); }
+            set { SetValue(StoredHeightProperty, value); }
+        }
+        #endregion
+
+
+    }
+
+
+    public class MarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var margin = (double)value;
+                var direction = (string)parameter;
+                switch (direction)
+                {
+                    case "Left":
+                        return new Thickness(-margin, 0, 0, 0); // 8 is the width of the expand button itself
+                    case "Right":
+                        return new Thickness(0, 0, -margin, 0); // 8 is the width of the expand button itself
+                    case "Up":
+                        return new Thickness(0, -margin, 0, 0);
+                    case "Down":
+                        return new Thickness(0, 0, 0, -margin);
+                    default:
+                        return new Thickness(0);
+                }
+            }
+            catch (Exception)
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back from a replacement!");
+        }
+    }
+}
---
+++ ClientControls/SearchBox.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientControls
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Represents a Metro UI search control.
+    /// </summary>
+    /// <remarks>
+    /// Public Properties:
+    ///     PlaceholderText       : string
+    ///     ListBackground        : Brush
+    ///     ListForeground        : Brush
+    ///     ButtonHoverBackground : Brush
+    ///     ButtonHoverForeground : Brush
+    ///     SelectedBackground    : Brush
+    ///     SelectedForeground    : Brush
+    ///     SeperatorColor        : Brush
+    ///     
+    /// Public Events:
+    ///     SearchClick           : Bubble
+    ///     SettingBtnClick       :
+    ///     SettingBtnChecked
+    /// </remarks>
+    public class SearchBox : ComboBox
+    {
+        /// <summary>
+        /// Initializes a new instance of the SearchBox class.
+        /// </summary>
+        public SearchBox()
+            : base()
+        {
+            DefaultStyleKey = typeof(SearchBox);
+        }
+
+
+        /// <summary>
+        /// Adds or removes the event handler 
+        /// </summary>
+        public event RoutedEventHandler SettingBtnChecked
+        {
+            add { AddHandler(SettingBtnCheckedEvent, value); }
+            remove { RemoveHandler(SettingBtnCheckedEvent, value); }
+        }
+
+        /// <summary>
+        /// Routed event register for the SettingBtnChecked event.
+        /// </summary>
+        public static readonly RoutedEvent SettingBtnCheckedEvent =
+            EventManager.RegisterRoutedEvent("SettingBtnChecked",
+                                              RoutingStrategy.Bubble,
+                                              typeof(RoutedEventHandler),
+                                              typeof(SearchBox)
+                                            );
+
+
+
+        /// <summary>
+        /// Adds or removes the event handler 
+        /// </summary>
+        public event RoutedEventHandler SettingBtnClick
+        {
+            add { AddHandler(SettingBtnClickEvent, value); }
+            remove { RemoveHandler(SettingBtnClickEvent, value); }
+        }
+
+        /// <summary>
+        /// Routed event register for the SettingBtnClick event.
+        /// </summary>
+        public static readonly RoutedEvent SettingBtnClickEvent =
+            EventManager.RegisterRoutedEvent("SettingBtnClick",
+                                              RoutingStrategy.Bubble,
+                                              typeof(RoutedEventHandler),
+                                              typeof(SearchBox)
+                                            );
+
+
+
+        /// <summary>
+        /// Adds or removes the event handler for the TabCloseClick event.
+        /// </summary>
+        public event RoutedEventHandler SearchClick
+        {
+            add { AddHandler(SearchClickEvent, value); }
+            remove { RemoveHandler(SearchClickEvent, value); }
+        }
+
+        /// <summary>
+        /// Routed event register for the SearchClick event.
+        /// </summary>
+        public static readonly RoutedEvent SearchClickEvent =
+            EventManager.RegisterRoutedEvent("SearchClick",
+                                              RoutingStrategy.Bubble,
+                                              typeof(RoutedEventHandler),
+                                              typeof(SearchBox)
+                                            );
+
+
+        /// <summary>
+        /// Gets or sets the placeholder text value.
+        /// </summary>
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for SearchHolderText.
+        /// </summary>
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked",
+                                         typeof(bool),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(false)
+                                       );
+
+        /// <summary>
+        /// Gets or sets the placeholder text value.
+        /// </summary>
+        public bool IsShowSettingButton
+        {
+            get { return (bool)GetValue(IsShowSettingButtonProperty); }
+            set { SetValue(IsShowSettingButtonProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for SearchHolderText.
+        /// </summary>
+        public static readonly DependencyProperty IsShowSettingButtonProperty =
+            DependencyProperty.Register("IsShowSettingButton",
+                                         typeof(bool),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(false)
+                                       );
+
+
+        /// <summary>
+        /// Gets or sets the placeholder text value.
+        /// </summary>
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for SearchHolderText.
+        /// </summary>
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.Register("PlaceholderText",
+                                         typeof(string),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(string.Empty)
+                                       );
+
+        /// <summary>
+        /// Gets or sets the dropdown list background color.
+        /// </summary>
+        public Brush ListBackground
+        {
+            get { return (Brush)GetValue(ListBackgroundProperty); }
+            set { SetValue(ListBackgroundProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for ListBackground.
+        /// </summary>
+        public static readonly DependencyProperty ListBackgroundProperty =
+            DependencyProperty.Register("ListBackground",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 255, 255, 255)))
+                                       );
+
+        /// <summary>
+        /// Gets or sets the dropdown list foreground color.
+        /// </summary>
+        public Brush ListForeground
+        {
+            get { return (Brush)GetValue(ListForegroundProperty); }
+            set { SetValue(ListForegroundProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for ListForeground.
+        /// </summary>
+        public static readonly DependencyProperty ListForegroundProperty =
+            DependencyProperty.Register("ListForeground",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 51, 51, 51)))
+                                       );
+
+        /// <summary>
+        /// Gets or sets the button background color while in the hover-over state.
+        /// </summary>
+        public Brush ButtonHoverBackground
+        {
+            get { return (Brush)GetValue(ButtonHoverBackgroundProperty); }
+            set { SetValue(ButtonHoverBackgroundProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for ButtonHoverBackground.
+        /// </summary>
+        public static readonly DependencyProperty ButtonHoverBackgroundProperty =
+            DependencyProperty.Register("ButtonHoverBackground",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 27, 161, 226)))
+                                       );
+
+        /// <summary>
+        /// Gets or sets the button foreground color while in the hover-over state.
+        /// </summary>
+        public Brush ButtonHoverForeground
+        {
+            get { return (Brush)GetValue(ButtonHoverForegroundProperty); }
+            set { SetValue(ButtonHoverForegroundProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for ButtonHoverForeground.
+        /// </summary>
+        public static readonly DependencyProperty ButtonHoverForegroundProperty =
+            DependencyProperty.Register("ButtonHoverForeground",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 255, 255, 255)))
+                                       );
+
+        /// <summary>
+        /// Gets or sets the list item background color while in the selected state.
+        /// </summary>
+        public Brush SelectedBackground
+        {
+            get { return (Brush)GetValue(SelectedBackgroundProperty); }
+            set { SetValue(SelectedBackgroundProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for SelectedBackground.
+        /// </summary>
+        public static readonly DependencyProperty SelectedBackgroundProperty =
+            DependencyProperty.Register("SelectedBackground",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 27, 161, 226)))
+                                       );
+
+        /// <summary>
+        /// Gets or sets the list item foreground color while in the selected state.
+        /// </summary>
+        public Brush SelectedForeground
+        {
+            get { return (Brush)GetValue(SelectedForegroundProperty); }
+            set { SetValue(SelectedForegroundProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for SelectedForeground.
+        /// </summary>
+        public static readonly DependencyProperty SelectedForegroundProperty =
+            DependencyProperty.Register("SelectedForeground",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 255, 255, 255)))
+                                       );
+
+        /// <summary>
+        /// Gets or sets the list item separator color.
+        /// </summary>
+        public Brush SeparatorColor
+        {
+            get { return (Brush)GetValue(SeparatorColorProperty); }
+            set { SetValue(SeparatorColorProperty, value); }
+        }
+
+        /// <summary>
+        /// Dependency Property for SeparatorColor.
+        /// </summary>
+        public static readonly DependencyProperty SeparatorColorProperty =
+            DependencyProperty.Register("SeparatorColor",
+                                         typeof(Brush),
+                                         typeof(SearchBox),
+                                         new PropertyMetadata(
+                                             new SolidColorBrush(
+                                                 Color.FromArgb(255, 204, 204, 204)))
+                                       );
+
+        private void searchButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //if (this.Text != string.Empty)
+            //{
+            //    if (!this.Items.Contains(this.Text))
+            //    {
+            //        this.Items.Add(this.Text);
+            //    }
+            //}
+
+            this.RaiseEvent(new RoutedEventArgs(SearchClickEvent, this));
+        }
+
+
+
+        private void SettingBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IsChecked = !IsChecked;
+
+            this.RaiseEvent(new RoutedEventArgs(SettingBtnClickEvent, this));
+        }
+
+
+        private void SettingBtn_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.RaiseEvent(new RoutedEventArgs(SettingBtnCheckedEvent, this));
+        }
+
+
+
+
+        /// <summary>
+        /// Invoked whenever application code or internal processes call ApplyTemplate.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            Button searchButton = base.GetTemplateChild("BtnSearch") as Button;
+            if (searchButton != null)
+            {
+                searchButton.Click += new RoutedEventHandler(searchButton_Click);
+            }
+
+            ToggleButton settingBtn = base.GetTemplateChild("ToggleBtnSetting") as ToggleButton;
+            if (settingBtn != null)
+            {
+                settingBtn.Click += new RoutedEventHandler(SettingBtn_Click);
+                settingBtn.Checked += new RoutedEventHandler(SettingBtn_Checked);
+            }
+
+
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            // Let ComboBox's original method handle it
+            /*base.OnPreviewKeyDown(e);
+
+            if (e.Key == Key.Enter)
+                e.Handled = false;
+            return;*/
+        }
+
+    }
+}
