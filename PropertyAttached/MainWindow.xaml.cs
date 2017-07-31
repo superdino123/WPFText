3814f4bc1a5a586e294f8495e6d1860ed7c6f1b4
diplant <1119674493@qq.com>
2017-07-31 11:33:57 +0800

    依赖属性联系

---
+++ PropertyAttached/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿ 
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PropertyAttached
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeLayout();
+        }
+
+        private void InitializeLayout()
+        {
+            Grid grid = new Grid() {ShowGridLines = true};
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+
+            Button button = new Button() {Content = "OK"};
+            Grid.SetColumn(button,1);
+            Grid.SetRow(button,1);
+            grid.Children.Add(button);
+            this.Content = grid;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Human human = new Human();
+            School.SetGrade(human,6);
+            int grade = School.GetGrade(human);
+            MessageBox.Show(grade.ToString());
+        }
+    }
+
+    class School : DependencyObject
+    {
+        //自定义附加属性
+        public static readonly DependencyProperty GradeProperty = 
+            DependencyProperty.RegisterAttached("Grade",typeof(int),typeof(School),new UIPropertyMetadata(0));
+
+        public static int GetGrade(DependencyObject obj)
+        {
+            return (int) obj.GetValue(GradeProperty);
+        }
+
+        public static void SetGrade(DependencyObject obj, int value)
+        {
+            obj.SetValue(GradeProperty,value);
+        }
+    }
+
+    class Human : DependencyObject
+    {
+
+    }
+}
---
+++ PropertyObject/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PropertyObject
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student stu;
+        public MainWindow()
+        {
+            InitializeComponent();
+            stu = new Student();
+            stu.SetBinding(Student.NameProperty, new Binding("Text") {Source = TextBox1});
+            TextBox2.SetBinding(TextBox.TextProperty, new Binding("Name") {Source = stu});
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //没有属性包装器时
+            //Student stu = new Student();
+            //stu.SetValue(Student.NameProperty,this.TextBox1.Text);
+            //TextBox2.Text = (string) stu.GetValue(Student.NameProperty);
+            
+            //有了属性包装器后
+            Student stu = new Student();
+            stu.Name = this.TextBox1.Text;
+            this.TextBox2.Text = stu.Name;
+        }
+    }
+
+    public class Student : DependencyObject
+    {
+        /// <summary>
+        /// 声明依赖属性
+        /// 参数(包装该属性的CLR属性，该属性的类型，该属性的宿主的类型)
+        /// </summary>
+        public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name",typeof(string),typeof(Student));
+
+        /// <summary>
+        /// CLR属性包装器
+        /// 为啥要用属性包装器：因为直接用依赖属性需要用GetValue和属性转换，看起来不方便
+        /// 达到效果：
+        /// 原来这么写 (string) stu.GetValue(Student.NameProperty);
+        /// 现在这么些 stu.Name
+        /// </summary>
+        public string Name
+        {
+            get { return (string) GetValue(NameProperty); }
+            set { SetValue(NameProperty,value);}
+        }
+
+        public BindingExpressionBase SetBinding(DependencyProperty dp, BindingBase binding)
+        {
+            return BindingOperations.SetBinding(this,dp,binding);
+        }
+    }
+}
